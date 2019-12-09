7729aad670341a211896891de3f69adf8cecccc6
zhuyingjie <m6567fc@outlook.com>
2019-12-09 20:57:34 +0800

    Update LazyByteArray.java

--- src/main/java/org/tdf/rlp/LazyByteArray.java
+++ src/main/java/org/tdf/rlp/LazyByteArray.java
@@ -3,25 +3,25 @@ package org.tdf.rlp;
 import java.util.Arrays;
 
 // reduce byte array copy by lazy loading
-public class LazyByteArray {
+class LazyByteArray {
     private byte[] data;
     private int offset;
     private int limit;
 
     static LazyByteArray EMPTY = new LazyByteArray(new byte[0]);
 
-    public LazyByteArray(byte[] data){
+    LazyByteArray(byte[] data){
         this.data = data;
         this.limit = data.length;
     }
 
-    public LazyByteArray(byte[] data, int offset, int limit) {
+    LazyByteArray(byte[] data, int offset, int limit) {
         this.data = data;
         this.offset = offset;
         this.limit = limit;
     }
 
-    public byte[] get(){
+    byte[] get(){
         if(offset == 0 && limit == data.length) return data;
         data = Arrays.copyOfRange(data, offset, limit);
         offset = 0;
@@ -29,7 +29,7 @@ public class LazyByteArray {
         return data;
     }
 
-    public int size(){
+    int size(){
         return limit - offset;
     }
 }
