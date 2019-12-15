b293f3b848bbf23a847c679c057ab08ed8df6b4c
zhuyingjie <m6567fc@outlook.com>
2019-12-15 22:16:57 +0800

    Update ContainerTests.java

--- src/test/java/org/tdf/rlp/ContainerTests.java
+++ src/test/java/org/tdf/rlp/ContainerTests.java
@@ -5,17 +5,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RunWith(JUnit4.class)
 public class ContainerTests {
-    private static interface TestMap1 extends Map<ByteArrayMap<Map<String, List<Integer>>>, Collection<Map<byte[], ByteArraySet>>>{}
+    private static interface TestMap1<A, B, C> extends Map<ByteArrayMap<A>, Collection<Map<C, Set<B>>>>{}
+
+    private static interface TestMap2<D> extends TestMap1<Set<String>, ArrayList<byte[]>, D>{}
+
+    private static interface TestMap3 extends TestMap2<ByteArraySet>{}
+
     @Test
-    @Ignore
     public void test0(){
-        Container container = RLPUtils.fromClass(TestMap1.class);
-        Container container2 = RLPUtils.fromType(TestMap1.class.getGenericInterfaces()[0]);
+        Container container = Container.fromNoGeneric(TestMap3.class);
     }
 }
