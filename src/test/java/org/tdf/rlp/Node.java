76b6e621aad5c10b88dfe6f13489c80bec7c3c8c
zhuyingjie <m6567fc@outlook.com>
2019-12-16 23:20:33 +0800

    Update Node.java

--- src/test/java/org/tdf/rlp/Node.java
+++ src/test/java/org/tdf/rlp/Node.java
@@ -1,6 +1,5 @@
 package org.tdf.rlp;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.ArrayList;
@@ -8,7 +7,8 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
-public class Node{
+@RLPDecoding(Node.Decoder.class)
+public class Node {
     // RLP annotation specify the order of field in encoded list
     @RLP(0)
     public String name;
@@ -23,17 +23,17 @@ public class Node{
         this.name = name;
     }
 
-    public void addChildren(Collection<Node> nodes){
-        if(children == null){
+    public void addChildren(Collection<Node> nodes) {
+        if (children == null) {
             children = new ArrayList<>();
         }
-        if (!nodes.stream().allMatch(x -> x != this)){
+        if (!nodes.stream().allMatch(x -> x != this)) {
             throw new RuntimeException("tree like object cannot add self as children");
         }
         children.addAll(nodes);
     }
 
-    private static class Nested{
+    private static class Nested {
         @RLP
         private List<List<List<String>>> nested;
 
@@ -41,8 +41,21 @@ public class Node{
         }
     }
 
+    public static class Decoder implements RLPDecoder<Node> {
+        @Override
+        public Node decode(RLPElement element) {
+            Node n = new Node();
+            n.name = element.get(0).asString();
+            if (element.get(1).isNull()) return n;
+            n.children = new ArrayList<>(element.get(1).size());
+            for (int i = 0; i < element.get(1).size(); i++) {
+                n.children.add(decode(element.get(1).get(i)));
+            }
+            return n;
+        }
+    }
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         boolean lazy = true;
         Node root = new Node("1");
         root.addChildren(Arrays.asList(new Node("2"), new Node("3")));
@@ -58,15 +71,15 @@ public class Node{
         System.out.println("rlp size " + encoded.length);
         System.out.println("json size " + json.length);
         long start = System.currentTimeMillis();
-        for(int i = 0; i < 100000; i++){
+        for (int i = 0; i < 100000; i++) {
             // read as rlp tree
-            RLPElement.fromEncoded(encoded, false);
+            RLPElement.fromEncoded(encoded, true).as(Node.class);
         }
 
         long end = System.currentTimeMillis();
         System.out.println(end - start + " ms");
         start = System.currentTimeMillis();
-        for(int i = 0; i < 100000; i++){
+        for (int i = 0; i < 100000; i++) {
             // read as rlp tree
             mapper.readValue(json, Node.class);
         }
@@ -74,7 +87,7 @@ public class Node{
         System.out.println(end - start + " ms");
     }
 
-    public static void assertTrue(boolean b){
-        if(!b) throw new RuntimeException("assertion failed");
+    public static void assertTrue(boolean b) {
+        if (!b) throw new RuntimeException("assertion failed");
     }
 }
