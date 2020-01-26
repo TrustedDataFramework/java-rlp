a5cd26a578423caa68903e6c8dcc05ba81e9c1c4
zhuyingjie <m6567fc@outlook.com>
2020-01-26 10:13:49 +0800

    Create RLPMapper.java

---
+++ src/main/java/org/tdf/rlp/RLPMapper.java
@@ -0,0 +1,29 @@
+package org.tdf.rlp;
+
+import lombok.With;
+
+public class RLPMapper {
+    private RLPContext context = RLPContext.EMPTY;
+
+    public RLPElement readRLPTree(Object o){
+        return RLPElement.readRLPTree(o, context);
+    }
+
+    public byte[] encode(Object o){
+        return RLPCodec.encode(o, context);
+    }
+
+    public <T> T decode(RLPElement el, Class<T> clazz){
+        return RLPCodec.decode(el, clazz, context);
+    }
+
+    public <T> T decode(byte[] data, Class<T> clazz){
+        return RLPCodec.decode(data, clazz, context);
+    }
+
+    public RLPMapper withContext(RLPContext context) {
+        RLPMapper ret = new RLPMapper();
+        ret.context = context;
+        return ret;
+    }
+}
